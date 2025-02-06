94e76db76f774b53f574e727d3d2f89e0fde20d6
josephinerijpens <josy.rijpens@gmail.com>
2025-02-06 13:23:10 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
 basic.forever(function () {
-	
+    let kompas = 0
+    if (kompas <= 45 && kompas >= 135) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (kompas <= 135 && kompas >= 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (kompas <= 225 && kompas >= 315) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (kompas <= 315 && kompas < 0) {
+    	
+    }
+    if (kompas < 0 && kompas < 0) {
+    	
+    }
 })
